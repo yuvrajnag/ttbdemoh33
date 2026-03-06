4b6e72c778357d444df602b7ba31d7b03b9f96d9
Your Name <you@example.com>
2026-03-06 11:27:01 +0530

    Restore missing JPA imports in Users.java

--- src/main/java/com/example/shantifanrender/Users.java
+++ src/main/java/com/example/shantifanrender/Users.java
@@ -1,5 +1,9 @@
 package com.example.shantifanrender;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
